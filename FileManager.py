aa4a894eb030c50126a4d86700482b3e4227b03b
nbacchus-eagle-org <157850633+nbacchus-eagle-org@users.noreply.github.com>
2024-08-13 08:41:24 -0500

    File Manager Python File Upload

---
+++ FileManager.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass, field
+from tkinter import Tk     # from tkinter import Tk for Python 3.x
+from tkinter.filedialog import askopenfilename
+import os
+import pandas as pd
+
+@dataclass
+class FileManager():
+    filename : str = field(init=False, repr=False)
+
+    def pickFile(self, sheet_name):
+        Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+        self.filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+        
+        file = self.openFile(sheet_name)
+        
+        return file
+    
+    def openFile(self, sheet_name:str = "Sheet1"):
+        file_list = self.filename.split(".")
+        file_extension = file_list[-1]
+
+        file = None
+        if file_extension == "xlsx":
+            file = pd.read_excel(self.filename, header=0, sheet_name=sheet_name)
+        elif file_extension == "csv":
+            file = pd.read_csv(self.filename, encoding='utf-8')
+            file = file.fillna("")
+        
+        return file
+    
+    def formatLocalFilePath(self, filename):
+        self.filename = os.path.abspath(filename)
+    
+    def writeFile(self, filename, file_extension, file):
+        self.filename = os.path.abspath(filename)
+        file_path = self.filename + "." + file_extension
+        try:
+            file.to_csv(file_path, encoding="utf-8", index=False)
+        except Exception as e:
+                print(f"Unexpected {e=}, {type(e)=}")
+                raise
+        return
